10cbc2830cf37ffab1494d8b08a73b9af9500e8d
s.saletti <s.saletti@qzrstudio.com>
2024-06-13 13:53:00 +0200

    push

--- components/DisclaimerDialog.tsx
+++ components/DisclaimerDialog.tsx
@@ -6,15 +6,19 @@ const DisclaimerDialog = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	useEffect(() => {
-		const hasSeenDisclaimer = localStorage.getItem("hasSeenDisclaimer");
-		if (!hasSeenDisclaimer) {
-			setIsOpen(true);
+		if (typeof window !== "undefined") {
+			const hasSeenDisclaimer = localStorage.getItem("hasSeenDisclaimer");
+			if (!hasSeenDisclaimer) {
+				setIsOpen(true);
+			}
 		}
 	}, []);
 
 	const handleClose = () => {
-		localStorage.setItem("hasSeenDisclaimer", "true");
-		setIsOpen(false);
+		if (typeof window !== "undefined") {
+			localStorage.setItem("hasSeenDisclaimer", "true");
+			setIsOpen(false);
+		}
 	};
 
 	return (
@@ -22,13 +26,13 @@ const DisclaimerDialog = () => {
 			{isOpen && (
 				<div className='fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50'>
 					<div className='modal modal-open'>
-						<div className='modal-box !bg-judge-gray-800  max-w-xl'>
-							<div className='font-bold text-3xl !text-hampton-200'>Disclaimer</div>
+						<div className='modal-box !bg-judge-gray-800 max-w-xl'>
+							<div className='font-bold text-lg text-hampton-200'>Disclaimer</div>
 							<div className='py-4 text-hampton-200'>
-								<p className='mb-4'>I am not affiliated with Wayfinder in any way.</p>
 								<p className='mb-4'>
-									The data provided on this site could be incorrect. Please do not use it for making
-									financial decisions.
+									I am not affiliated with Wayfinder. The data provided on this site could be
+									incorrect. Please do not use it for making financial decisions. Always consult with
+									a financial advisor or conduct your own research before making investment choices.
 								</p>
 							</div>
 							<div className='modal-action'>
