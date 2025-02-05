6eac4cc4ffdb02c02e12fd74296a4b8a6e2ccc0f
Krumil <salettisimone10@gmail.com>
2025-02-05 17:07:31 +0100

    push

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
 import CardStack from "@/components/CardStack";
 import { AnimatedNumber } from "@/components/ui/AnimatedNumber";
 import Loader from "@/components/Loader/Loader";
@@ -580,7 +579,7 @@ const Dashboard = ({
 										>
 											<div className="flex flex-col items-center text-center">
 												<div className="w-8 h-8">
-													<Image
+													<img
 														src={`https://staticfiles.wayfinder.ai/${badge.image}`}
 														alt={badge.name}
 														width={32}
